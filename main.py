d8e65ba10f20f0d44be7d35e4d1902c05bb8046f
Benjamin anagha <99402870+BenjDvs@users.noreply.github.com>
2026-08-17 18:21:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ async def handler(event):
         request = {
             "action": mt5.TRADE_ACTION_DEAL,
             "symbol": symbol,
-            "volume": 0.01, 
+            "volume": 0.02, 
             "type": order_type,
             "price": price,
             "sl": extracted_data['sl'],
