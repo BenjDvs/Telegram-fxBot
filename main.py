7737ffa6a8dd8d30317db8c9c16e8d69f89a4566
Benjamin anagha <99402870+BenjDvs@users.noreply.github.com>
2026-08-21 08:53:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def parse_signal(message_text):
     return signal_data
 
 client = TelegramClient('forex_session', api_id, api_hash)
-channel_username = 'goldkillerhub'
+channel_username = 'goodbestsignal'
 
 @client.on(events.NewMessage(chats=channel_username))
 async def handler(event):
